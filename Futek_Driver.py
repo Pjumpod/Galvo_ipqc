aecf7ff2ce9bac7eac9463efbc291c8b1a610926
JumpodLenovo <pjumpod@gmail.com>
2025-11-26 16:05:59 +0700

    fix bug in query

--- Futek_Driver.py
+++ Futek_Driver.py
@@ -54,10 +54,11 @@ def measSensor(zerotorque:float, ps_visa: str, ps_baud: int, prefix_log:str, ser
             measData = max(measSensor)
             start_log = "forward,"
         for num in measSensor:
-            data_to_log.append((measSensor[num] * 7.0616) - zerotorque)
+            data_to_log.append((num * 7.0616) - zerotorque)
         line = start_log + ",".join(map(str, data_to_log))
-        with open(logfile, 'a+', newline='') as file:
-            file.writelines(line)
+        if zerotorque != 0:
+            with open(logfile, 'a+', newline='') as file:
+                file.writelines(line)
         measData = (measData * 7.0616) - zerotorque
         return float(measData)
     else:
